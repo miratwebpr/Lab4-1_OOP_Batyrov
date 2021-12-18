f3386147522fd83059b8237ba68abe9b91dc1031
spapua228М <spapua228@mail.ru>
2021-12-18 17:21:41 +0500

    storage class added, adding circles added

--- Lab4-1_OOP_Batyrov/Form1.cs
+++ Lab4-1_OOP_Batyrov/Form1.cs
@@ -24,40 +24,6 @@ namespace Lab4_1_OOP_Batyrov
 
         private void sheet_MouseClick(object sender, MouseEventArgs e)
         {
-            int selected1 = -1;
-            if (V.getCount() == 0)
-            {
-                V.pushBack(new Circle(e.X, e.Y));
-                G.drawCircle(e.X, e.Y, V.getCount().ToString());
-                sheet.Image = G.GetBitmap();
-            }
-            for(int i = 0; i < V.getCount(); i++)
-            {
-                if(Math.Pow((V[i].x - e.X), 2) + Math.Pow((V[i].y - e.Y), 2) <= G.R * G.R)
-                {
-                    if (selected1 != -1)
-                    {
-                        selected1 = -1;
-                        G.clearSheet();
-                        G.drawALLGraph(V);
-                        sheet.Image = G.GetBitmap();
-                    }
-                    if (selected1 == -1)
-                    {
-                        G.drawSelectedCircle(V[i].x, V[i].y);
-                        selected1 = i;
-                        sheet.Image = G.GetBitmap();
-                        return;
-                    }
-                }
-                else
-                {
-                    V.pushBack(new Circle(e.X, e.Y));
-                    G.drawCircle(e.X, e.Y, V.getCount().ToString());
-                    sheet.Image = G.GetBitmap();
-                    return;
-                }
-            }
-        }
+            
     }
 }
